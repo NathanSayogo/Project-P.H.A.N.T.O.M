6abb1445bec6099422a353e1c487e231ab79af37
Nathan <F2c4g8@gmail.com>
2026-05-24 20:11:47 -0400

    Regfile

---
+++ Regfile.sv
@@ -0,0 +1,14 @@
+module Regfile(
+	input clk, RegWen,
+  	input [4:0] AddrD, AddrA, AddrB,
+  	input [31:0] DataD,
+  	output [31:0] DataA, DataB
+);
+  logic [31:0] registers [31:0];
+  assign DataA = registers[AddrA];
+  assign DataB = registers[AddrB];
+  always_ff @(posedge clk) begin
+    if(RegWen && AddrD != 0)
+      registers[AddrD] <= DataD;
+  end
+endmodule
