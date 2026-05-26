a54f74e2c4b4b80df262ad27a94218aac74d08cb
Nathan Davison Sayogo <ndsayogo@uwaterloo.ca>
2026-05-26 15:18:29 -0400

    feat: implement Immediate Generator with support for all RV32I immediate formats (fix: added 'endmodule')

--- Imm_Gen.sv
+++ Imm_Gen.sv
@@ -10,9 +10,10 @@ always_comb begin
         7'b1100111,
         7'b0010011: imm = {{20{inst[31]}}, inst[31:20]};
         7'b0100011: imm = {{20{inst[31]}}, inst[31:25],inst[11:7]};
-        7'b1100011: imm = {{20{inst[31]}}, inst[7], inst[31:25],inst[11:8], 1'b0};
+        7'b1100011: imm = {{20{inst[31]}}, inst[7], inst[30:25],inst[11:8], 1'b0};
         7'b1101111: imm = {{12{inst[31]}}, inst[19:12], inst[20], inst[30:21], 1'b0};
         7'b0110111,
         7'b0010111: imm = {inst[31:12], 12'b0};
     endcase
 end
+endmodule
